63ca7871bd57fe64192e96ce9fc67066ae51b3a7
Jungpil YU <jungpilyu@users.noreply.github.com>
2018-11-28 00:25:42 +0900

    [feature] smooth player movement

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+// global entity variables
+let allEnemies = [];
+let player;
+const playerSpeed = 500;
+
 // Enemies our player must avoid
 let Enemy = function (y, speed) {
     // Variables applied to each of our instances go here,
@@ -6,7 +11,7 @@ let Enemy = function (y, speed) {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.y = y; // {1,2,3}
+    this.y = y; // {1,2,3}*83
     this.reset(speed);
 };
 
@@ -30,27 +35,31 @@ Enemy.prototype.update = function(dt) {
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y*83);
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function (sprite) {
+var Player = function (sprite = 'images/char-princess-girl.png') {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = sprite || 'images/char-princess-girl.png';
+    this.sprite = sprite;
     this.reset = () => {
       this.x = 101*2;
-      this.y = 5;
+      this.x2 = 101*2;
+      this.y = 5*83;
+      this.y2 = 5*83;
     }
     this.reset();
 
+
     // the player methods of 'update', 'reset', 'handleInput' are not defined
-    // in the prototype link because there will be only one player instance.
+    // in the prototype link because there will be only one player instance
+    // and no gain is obtained by define them in the prototype link.
     this.update = function(dt) {
       // check if the player collides the enemy
       for (enemy of allEnemies) {
@@ -66,17 +75,32 @@ var Player = function (sprite) {
           alert('You win!');
         }, 0);
       }
+      // move the player
+      const move = playerSpeed * dt;
+      const dx = this.x2 - this.x;
+      const dy = this.y2 - this.y;
+
+      if(dx > 0) {
+        this.x += (dx < move ? dx : move);
+      } else {
+        this.x -= (-dx < move ? -dx : move);
+      }
+      if(dy > 0) {
+        this.y += (dy < move ? dy : move);
+      } else {
+        this.y -= (-dy < move ? -dy : move);
+      }
     }
 
     this.handleInput = function(key) {
-      if(key == 'left' && this.x - 101 >= 0) {
-        this.x -= 101;
-      } else if (key == 'up' && this.y - 1 >= 0) {
-        this.y -= 1;
-      } else if (key == 'right' && this.x + 101 < 101*5) {
-        this.x += 101;
-      } else if (key == 'down' && this.y + 1 <= 5) {
-        this.y += 1;
+      if(key == 'left' && this.x2 - 101 >= 0) {
+        this.x2 -= 101;
+      } else if (key == 'up' && this.y2 - 83 >= 0) {
+        this.y2 -= 83;
+      } else if (key == 'right' && this.x2 + 101 < 101*5) {
+        this.x2 += 101;
+      } else if (key == 'down' && this.y2 + 83 <= 5*83) {
+        this.y2 += 83;
       }
     }
 };
@@ -87,16 +111,12 @@ Player.prototype = Enemy.prototype;
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-// global entity variables
-let allEnemies = [];
-let player;
-
 // this function is called from the 'reset()' function in 'engine.js' file
 function intantiateEntities(level = 2) {
   for(let i = 0; i < level; i++) {
-    allEnemies.push(new Enemy(1, Math.random()*300 + 100));
-    allEnemies.push(new Enemy(2, Math.random()*300 + 100));
-    allEnemies.push(new Enemy(3, Math.random()*300 + 100));
+    allEnemies.push(new Enemy(1*83, Math.random()*300 + 100));
+    allEnemies.push(new Enemy(2*83, Math.random()*300 + 100));
+    allEnemies.push(new Enemy(3*83, Math.random()*300 + 100));
   }
   player = new Player();
 }
